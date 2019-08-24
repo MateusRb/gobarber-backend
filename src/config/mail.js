83f42c7c74f84cf12eb35aeb6927c4c2aedd55b2
Mateus Ribeiro <mateus_ribeiro21@hotmail.com>
2019-08-23 21:33:00 -0300

    adicionando envio de email, criando o cancelamento de agendamento e configurando os templates de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'eae919e72b563b',
+    pass: 'f99c96658f2fab',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
